5055e7c358b580fe4004063194b153a4001e29e3
tarmes <traarmes@gmail.com>
2020-12-14 16:24:58 -0500

    users-router set up

---
+++ api/users/users-model.js
@@ -0,0 +1,5 @@
+const db = require('../../database/db-config');
+
+module.exports = {
+   
+}
---
+++ api/users/users-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const Users = require('./users-model');
+
+const router = express.Router();
+
+module.exports = router;
