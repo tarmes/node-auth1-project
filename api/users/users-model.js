8f5cb5529e5a7214526d93cb6c3d162f54a5e8b7
tarmes <traarmes@gmail.com>
2020-12-14 22:27:04 -0500

    mvp

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,7 +1,19 @@
 const db = require('../../database/db-config');
 
+function findById(id) {
+   return db('users').where({ id }).first();
+}
+
 module.exports = {
    getAll() {
       return db('users');
-   }
+   },
+   findBy(filter) {
+      return db('users').where(filter).orderBy('id');
+   },
+   async add(user) {
+      const [id] = await db('users').insert(user, 'id');
+      return findById(id);
+   },
+   findById
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -4,7 +4,15 @@ const Users = require('./users-model');
  
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const restricted = (req, res, next) => {
+   if (req.session && req.session.user) {
+      next();
+   } else {
+      res.status(401).json('unauthorized');
+   }
+}
+
+router.get('/', restricted, (req, res) => {
    Users.getAll()
       .then(users => {
          res.status(200).json(users);
